a62eb5f6e3e7f759d1711c70653b2fe36371d9e6
Suraj <sncmath@gmail.com>
2020-01-06 10:46:20 +0530

    Suraj_AndroidTasks_Page12,16_V0.1

---
+++ HTML-TASKSV0.3/REQUISITION_FORM1/HTML/HTML_USING_TABLE/example.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int day;
+    printf ("enter any number from 1 to 7 only");
+    scanf ("%d",&day);
+    switch (day)
+case 1: printf("\n Monday")
+        break;
+case 2: printf("\n Tuesday")
+        break;
+case 3: printf("\n Wednesday")
+        break;
+case 4: printf("\n Thursday")
+        break;
+case 5: printf("\n Friday")
+        break;
+default:printf("\ninvalid")
+}
