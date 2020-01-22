2b539becb87c433430a13b10a2af46c38251c44c
Suraj <sncmath@gmail.com>
2020-01-22 10:55:21 +0530

    Suraj_Android_MDC

---
+++ ANDROID_TASKS/MyApplication/app/src/main/java/com/example/myapplication/my_request_diff.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_request_diff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_request_diff);
+    }
+}
